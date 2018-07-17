2cd25e38e4e22339a2487d54b8c704eb7e843c2f
田中良明 <tanakayoshiaki@tanaka-macbook.local>
2018-07-18 04:03:33 +0900

    Initial Commit

---
+++ Batoen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Batoen
+//
+//  Created by 田中良明 on 2018/07/18.
+//  Copyright © 2018年 tanakayoshiaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
